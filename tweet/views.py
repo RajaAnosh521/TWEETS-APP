30c0a6dc41212bc425c214c13fe4dfaf28a26c5c
RajaAnosh521 <javariaali070@gmail.com>
2024-09-11 14:20:37 +0500

    Initial commit

--- tweet/views.py
+++ tweet/views.py
@@ -26,6 +26,7 @@ def tweet_create(request):
         form = TweetForm()
 
     return render(request, 'tweet_form.html', {'form': form})
+
 @login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
